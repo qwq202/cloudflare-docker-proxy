179bfd747d97d7585050abadfb516418eb42f5ed
qunqin <146629280+qwq202@users.noreply.github.com>
2024-06-19 13:22:31 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const routes = {
   "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
   "k8s.libcuda.so": "https://registry.k8s.io",
   "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "cloudsmith.qunqin.org": "https://docker.cloudsmith.io",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
